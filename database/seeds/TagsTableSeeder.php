0e4ca8befcac610af712a0ea17253bf7dcbf8b63
shouma.itezono <zooonshou@gmail.com>
2021-04-14 11:29:26 +0000

    modify git heroku pull

--- database/seeds/TagsTableSeeder.php
+++ database/seeds/TagsTableSeeder.php
@@ -11,7 +11,6 @@ class TagsTableSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
         DB::table('tags')->where('content', '香りが良い')->delete();
         
         DB::table('tags')->insert([
@@ -66,26 +65,6 @@ class TagsTableSeeder extends Seeder
             'content' => '冷やし'
         ]);
         DB::table('tags')->insert([
-=======
-        DB::table('tags')->insert([
-            'content' => '激辛', 
-            'content' => '辛い', 
-            'content' => '辛さ控え目', 
-            'content' => '痺れる', 
-            'content' => '酸味あり', 
-            'content' => 'スパイシー', 
-            'content' => 'クリーミー', 
-            'content' => '濃厚', 
-            'content' => 'さっぱり', 
-            'content' => '細麺', 
-            'content' => '中太麺', 
-            'content' => '太麺', 
-            'content' => '縮れ麺', 
-            'content' => '黒胡麻', 
-            'content' => '白胡麻', 
-            'content' => '汁なし', 
-            'content' => '冷やし', 
->>>>>>> 7882c26a232ab0a623fada0d6decc5b6899dd77b
             'content' => '香りが良い', 
         ]);
     }
